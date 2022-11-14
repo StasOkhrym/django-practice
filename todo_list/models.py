afb5549e1001affb45e4811bc4ebd33a61e69aac
Stas Okhrym <s.okhrym@gmail.com>
2022-11-14 10:28:46 +0200

    Added models to todo_list app

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField()
+    is_completed = models.BooleanField()
+    tags = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.content
