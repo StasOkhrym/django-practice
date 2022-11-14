bd9bbe6aed4d13f5e44cce98d12e6e00e0497390
Stas Okhrym <s.okhrym@gmail.com>
2022-11-14 10:54:47 +0200

    Added templates and static & fill view for task list

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views import generic
 
+from todo_list.forms import TaskSearchForm
 from todo_list.models import Task
 
 
@@ -8,5 +8,27 @@ class TaskListView(generic.ListView):
     model = Task
     queryset = Task.objects.select_related("tags_id")
     paginate_by = 5
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(TaskListView, self).get_context_data(**kwargs)
+
+        name = self.request.GET.get("name", "")
+        not_completed = self.request.GET.get("not_completed", "")
+
+        context["search_form"] = TaskSearchForm(
+            initial={"name": name, "not_completed": not_completed}
+        )
+        return context
+
+    def get_queryset(self):
+        form = TaskSearchForm(self.request.GET)
+
+        if form.is_valid():
+            if form.cleaned_data["name"]:
+                return self.queryset.filter(
+                    content__icontains=form.cleaned_data["name"]
+                )
+            return self.queryset
+
 
 
